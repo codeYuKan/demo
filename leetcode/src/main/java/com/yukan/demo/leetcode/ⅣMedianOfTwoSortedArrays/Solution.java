1c15065b3dc816fd92351263d31a09de43a8ca47
Yu Kan <yukan@ddjf.com.cn>
2020-08-19 18:08:47 +0800

    1.leetcode 4 ⅣMedianOfTwoSortedArrays

--- leetcode/src/main/java/com/yukan/demo/leetcode/ⅣMedianOfTwoSortedArrays/Solution.java
+++ leetcode/src/main/java/com/yukan/demo/leetcode/ⅣMedianOfTwoSortedArrays/Solution.java
@@ -19,83 +19,147 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class Solution {
+
+    private static double getResult (int a, int b) {
+        if (needDivide) {
+            return (double) (a + b) / 2;
+        } else {
+            return Math.max(a, b);
+        }
+    }
+
+    private static double getResult (int a1, int a2, int b1, int b2) {
+        if (b2 >= a1) {
+            if (a2 >= b1) {
+                return getResult(a2, b2);
+            } else {
+                return getResult(b1, b2);
+            }
+        } else {
+            return getResult(a1, a2);
+        }
+    }
+
+    private static boolean needDivide;
+
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int aLength = nums1.length;
         int bLength = nums2.length;
         int length = aLength + bLength;
-        int resultOffset = 0;
-        boolean needDivide;
+        int leftNum;
+        //boolean needDivide;
         if (length % 2 == 0) {
-            resultOffset = length / 2;
+            // 偶数
+            leftNum = (length + 2) / 2;
             needDivide = true;
         } else {
-            resultOffset = (length + 1) / 2;
+            // 奇数
+            leftNum = (length + 1) / 2;
             needDivide = false;
         }
 
-        int aIndex = 0;
-        int bIndex = 0;
-        int aNum = nums1[aIndex];
-        int bNum = nums2[bIndex];
-
-        boolean aEnd = aIndex == aLength;
-        boolean bEnd = bIndex == bLength;
-
-        for(int i = 0; i < resultOffset - 1; i++) {
-            if (aEnd) {
-                // a已经全部被淘汰了
-
-            }
-            if (bEnd) {
-
-            }
-
-            if (aNum <= bNum) {
-                aIndex++;
-                aNum = nums1[aIndex];
-                aEnd = aIndex + 1 == aLength;
+        if (aLength == 0) {
+            if (needDivide) {
+                return (double) (nums2[leftNum - 1] + nums2[leftNum - 2]) / 2;
             } else {
-                bIndex++;
-                bNum = nums2[bIndex];
-                bEnd = bIndex + 1 == bLength;
+                return nums2[leftNum - 1];
             }
         }
+        if (bLength == 0) {
+            if (needDivide) {
+                return (double) (nums1[leftNum - 1] + nums1[leftNum - 2]) / 2;
+            } else {
+                return nums1[leftNum - 1];
+            }
+        }
+        if (aLength == 1 && bLength == 1) {
+            return (double) (nums1[0] + nums2[0]) / 2;
+        }
 
-        int divideTmp = 0;
-        if (aNum <= bNum) {
-            if (!needDivide) {
-                return aNum;
-            } else {
-                divideTmp =+ aNum;
-                aIndex++;
-                aNum = nums1[aIndex];
-                if (aNum <= bNum) {
-                    divideTmp =+ aNum;
-                } else {
-                    divideTmp =+ bNum;
+        if (aLength < bLength) {
+            int aIndex = aLength - 1;
+            int bIndex = leftNum - aIndex - 1;
+            int aNum = nums1[aIndex];
+            int bNum = nums2[bIndex];
+
+            if (aNum <= bNum) {
+                if (aLength == 1) {
+                    if (bIndex == 1) {
+                        return getResult(aNum, nums2[bIndex - 1]);
+                    }
+                    return getResult(Math.max(aNum, nums2[bIndex - 2]), nums2[bIndex - 1]);
+                } else if (bIndex == 1) {
+                    return getResult(Math.max(nums2[bIndex - 1], nums1[aIndex - 1]), aNum);
                 }
-                return (double) divideTmp / 2;
+                return getResult(nums1[aIndex - 1], aNum, nums2[bIndex - 2], nums2[bIndex - 1]);
+                //return getResult(aNum, Math.max(nums1[aIndex - 1], nums2[bIndex - 1]));
             }
+            while (aIndex > 0) {
+                aNum = nums1[--aIndex];
+                bNum = nums2[++bIndex];
+                if (aNum <= bNum) {
+                    return getResult(aNum, nums2[bIndex - 1]);
+                }
+            }
+            return getResult(nums2[bIndex - 1], nums2[bIndex]);
+        } else if (aLength == bLength) {
+            int aIndex = aLength - 1;
+            int bIndex = leftNum - aIndex - 1;
+            int aNum = nums1[aIndex];
+            int bNum = nums2[bIndex];
+
+
+            if (aNum <= bNum) {
+                //if (bIndex == 1) {
+                    if (nums1[aIndex - 1] >= nums2[bIndex - 1]) {
+                        return getResult(aNum, nums1[aIndex - 1]);
+                    } else {
+                        return getResult(aNum, nums2[bIndex - 1]);
+                    }
+                //}
+                //return getResult(nums1[aIndex - 1], aNum, nums2[bIndex - 2], nums2[bIndex - 1]);
+            }
+            while (aIndex > 1) {
+                aNum = nums1[--aIndex];
+                bNum = nums2[++bIndex];
+                if (aNum <= bNum) {
+                    return getResult(aNum, nums2[bIndex - 1]);
+                }
+            }
+            return getResult(nums1[0], nums2[bIndex]);
         } else {
-            if (!needDivide) {
-                return bNum;
-            } else {
-                divideTmp =+ bNum;
-                bIndex++;
-                bNum = nums2[bIndex];
-                if (aNum <= bNum) {
-                    divideTmp =+ aNum;
-                } else {
-                    divideTmp =+ bNum;
+            int bIndex = bLength - 1;
+            int aIndex = leftNum - bIndex - 1;
+            int aNum = nums1[aIndex];
+            int bNum = nums2[bIndex];
+
+
+            if (bNum <= aNum) {
+                if (bLength == 1) {
+                    if (aIndex == 1) {
+                        return getResult(bNum, nums1[aIndex - 1]);
+                    }
+                    return getResult(Math.max(bNum, nums1[aIndex - 2]), nums1[aIndex - 1]);
+                } else if ( aIndex == 1) {
+                    return getResult(Math.max(nums1[aIndex - 1], nums2[bIndex - 1]), bNum);
                 }
-                return (double) divideTmp / 2;
+                return getResult(nums2[bIndex - 1], bNum, nums1[aIndex - 2], nums1[aIndex - 1]);
+                //return getResult(bNum, Math.max(nums1[aIndex - 1], nums2[bIndex - 1]));
             }
+            while (bIndex > 0) {
+                bNum = nums2[--bIndex];
+                aNum = nums1[++aIndex];
+                if (bNum <= aNum) {
+                    return getResult(bNum, nums1[aIndex - 1]);
+                }
+            }
+            return getResult(nums1[aIndex - 1], nums1[aIndex]);
         }
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1,2};
-        int[] nums2 = {3,4};
+        int[] nums1 = {2,3};
+        int[] nums2 = {1,4};
         System.out.println(findMedianSortedArrays(nums1, nums2));
     }
 }
