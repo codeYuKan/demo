9d5e610058b303888985cd20bea4de96d3c22125
Yu Kan <yukan@ddjf.com.cn>
2021-03-11 17:55:00 +0800

    1.commit ⅤLongestPalindromicSubstring done

--- leetcode/src/main/java/com/yukan/demo/leetcode/ⅤLongestPalindromicSubstring/Solution.java
+++ leetcode/src/main/java/com/yukan/demo/leetcode/ⅤLongestPalindromicSubstring/Solution.java
@@ -2,6 +2,7 @@ package com.yukan.demo.leetcode.ⅤLongestPalindromicSubstring;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import sun.util.resources.in.CalendarData_in_ID;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -28,62 +29,82 @@ public class Solution {
 
     public static String longestPalindrome(String s) {
         char[] chars = s.toCharArray();
-        Map<Character, List<Integer>> charMap = new HashMap<>(chars.length);
-        List<Integer> tempIList;
+        Map<Character, Set<Integer>> charMap = new HashMap<>(chars.length);
+        Set<Integer> integerSet;
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
-            tempIList = charMap.get(c);
-            if (Objects.isNull(tempIList)) {
-                // 第一次遇到
-                //List<Integer> l = new LinkedList<>();
-                //l.add(i);
-                charMap.put(c, Stream.of(i).collect(Collectors.toList()));
+            integerSet = charMap.get(c);
+            if (Objects.isNull(integerSet)) {
+                // LinkedHashSet是有序的
+                Set<Integer> set = new LinkedHashSet<>();
+                set.add(i);
+                charMap.put(c, set);
             } else {
-                tempIList.add(i);
+                integerSet.add(i);
             }
         }
-        log.info("{}", JSON.toJSONString(charMap));
 
         int start = 0;
         int end = 0;
         int count = 1;
+        int tmpCount;
 
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
-            tempIList = charMap.get(c);
-            if (tempIList.size() <= 1) {
-                // 没有可能构成回文
-                break;
-            } else {
-                Iterator<Integer> iterator = tempIList.iterator();
-                iterator.next();
-                while (iterator.hasNext()) {
+            integerSet = charMap.get(c);
+            if (integerSet.size() > 1) {
+                // size大于1才有必要判断
+                a:
+                for (Integer endIndex : integerSet) {
+                    /*
+                    短路判断:长度是否超过现有第一
+                     */
+                    tmpCount = endIndex - i + 1;
+                    if (tmpCount <= count) {
+                        // 长度不超过现有则删除
+                        continue;
+                    }
 
+                    /*
+                    判断i到endIndex之间是否回文
+                     */
+                    // 三个数字或者两个数字 必定构成回文
+                    if (tmpCount <= 3) {
+                        // 判断是超过现有最长回文串
+                        start = i;
+                        end = endIndex;
+                        count = tmpCount;
+                        continue;
+                    }
+                    for (int x = i + 1, y = endIndex - 1; x <= y; x++, y--) {
+                        if (chars[x] != chars[y]) {
+                            // 不在则删除
+                            continue a;
+                        }
+                    }
+                    start = i;
+                    end = endIndex;
+                    count = tmpCount;
                 }
-                //for (int j = 1; j < tempIList.size(); j++) {
-                //    int endIndex = tempIList.get(j);
-                //    // 判断在i到endIndex之间
-                //
-                //
-                //    // 不在则删除
-                //    tempIList.remove(j);
-                //}
-
-
             }
-            // 不在则删除
-            tempIList.remove(0);
+            // 判断完 删除
+            integerSet.remove(i);
         }
 
 
-        return s;
+        return s.substring(start, end + 1);
     }
 
     public static void main(String[] args) {
-        //longestPalindrome("124dascasfcaswdfr12rf");
-        //List<Integer> l = Stream.of(3).collect(Collectors.toList());
-        //l.remove(0);
-        //System.out.println(l.size());
+//        //longestPalindrome("124dascasfcaswdfr12rf");
+//        //List<Integer> l = Stream.of(3).collect(Collectors.toList());
+//        //l.remove(0);
+//        //System.out.println(l.size());
+//Set<Integer> set = Stream.of(2,5,6,6,3,4,5,6).collect(Collectors.toSet());
+//        System.out.println(JSON.toJSONString(set));
+//        set.remove(3);
+//        System.out.println(JSON.toJSONString(set));
 
+        System.out.println(longestPalindrome("sxxxrjgrirnkkiafhdlzirazovyyutbysmteztuhezmfmrxwbsklykwukesocrprvlezocszhmpmfakxjxawsxirefkhoalqmgwqqgeemdvjigvmtorzobfqnkqrdytydglfvcinnhagehieoxpvufeutlmcgcsplwnaqmrtqtmxyrf"));
     }
 }
