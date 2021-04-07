a3a57cbb457cc1969cf241e0e461f489ba324a38
Yu Kan <yukan@ddjf.com.cn>
2021-04-07 15:14:40 +0800

    1.commit ⅩRegularExpressionMatching

--- leetcode/src/main/java/com/yukan/demo/leetcode/ⅩRegularExpressionMatching/Solution.java
+++ leetcode/src/main/java/com/yukan/demo/leetcode/ⅩRegularExpressionMatching/Solution.java
@@ -93,9 +93,46 @@ public class Solution {
         return sIndex == sLength;
     }
 
+
+
+    public static boolean isMatch2(String s, String p) {
+        int sLength = s.length();
+        int pLength = p.length();
+
+        boolean[][] f = new boolean[sLength + 1][pLength + 1];
+        f[0][0] = true;
+
+        for (int i = 0; i <= sLength; i++) {
+            for (int j = 1;j <= pLength; j++) {
+                if ('*' == p.charAt(j - 1)) {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+
+        return f[sLength][pLength];
+    }
+
+    private static boolean matches(String s,String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if ('.' == p.charAt(j - 1)) {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+
     public static void main(String[] args) {
         //System.out.println(isMatch("mississippi", "mis*is*p*."));
-        System.out.println(isMatch("issippi", "is*p*."));
+        System.out.println(isMatch2("issippi", "is*p*."));
         //System.out.println(isMatch("ippi", "p*."));
     }
 }
